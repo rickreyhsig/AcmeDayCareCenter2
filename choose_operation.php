001f0ef48561a0b26c70f4e0d5ee1941da7d0916
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2014-12-10 15:30:20 -0500

    Added operation routes

--- choose_operation.php
+++ choose_operation.php
@@ -1,27 +1,27 @@
 <html>
 <?php
-
 $operation = $_GET['operation'];
 
 if($operation=='enroll_child'){
   echo "Please enter information of the child that will be enrolled:";
-  echo "<form method=get action=enroll_child.php>
-  <input input=text name=fname value='First Name'><br>
-  <input input=text name=lname  value='Last Name'><br>
-  <input input=text name=phone value='Phone'><br>
-  <input input=text name=address value='Address'><br>
-  <input input=text name=email value='Email'><br>
-  <input input=text name=dob value='DOB (YYYY/MM/DD):'><br>
-  <input type=submit>";
+  echo '<form method=get action=enroll_child.php>
+  <input input=text size="30" name=child_fname value="Child\'s First Name"><br>
+  <input input=text size="30" name=child_lname value="Child\'s Last Name"><br>
+  <input input=text size="30" name=custodian_name  value="Custodian Name"><br>
+  <input input=text size="30" name=phone value="Phone"><br>
+  <input input=text size="30" name=address value="Address"><br>
+  <input input=text size="30" name=dob value="Child DOB (YYYY/MM/DD)"><br>
+  <input type=submit>';
 }elseif($operation=='assign_child'){
   echo "Please enter information of the child that will be assigned:";
   echo "<form method=get action=assign_child.php>
-  <input input=text name=fname value='First Name'><br>
+  <input input=text name=class_id value='Class ID'><br>
+  <input input=text name=child_id value='Child ID'><br>
   <input input=text name=lname  value='Last Name'><br>
   <input input=text name=phone value='Phone'><br>
   <input input=text name=address value='Address'><br>
   <input input=text name=email value='Email'><br>
-  <input input=text name=dob value='DOB (YYYY/MM/DD):'><br>
+  <input input=text name=dob value='DOB (YYYY/MM/DD)'><br>
   <input type=submit>";
 }elseif($operation=='list_children'){
   echo "Please enter information of the class you want to list the children for:";
